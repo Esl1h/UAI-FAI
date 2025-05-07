f7ef75fcdb9e48f21ecad5e7ff44999ef0598a2e
Esli Silva <not.announced@simplelogin.fr>
2025-05-07 14:26:49 -0300

    flatpak updates

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ function add_flathub {
 
 
 function flatpak_packages {
-    run_command "flatpak update -y" || error_exit "Failed to update flatpak"
+    run_command "flatpak update --appstream -y && flatpak remote-ls flathub > /dev/null" || error_exit "Failed to update flatpak"
     run_command "flatpak install flathub \
         com.protonvpn.www \
         org.standardnotes.standardnotes \
